bae71759cd53bb108397ad823fe9b80a46cbb10a
Thomas Burke <etburke4@gmail.com>
2018-01-10 15:27:28 -0800

    parameterize debug

--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 var redis = require('redis');
 var prerender = require('./lib');
 
+var LOG_REQUESTS = process.env.LOG_REQUESTS || false;
 var REDIS_URL = process.env.REDIS_URL;
 
 var server = prerender({
-  logRequests: true,
+  logRequests: LOG_REQUESTS,
   chromeLocation: '/usr/bin/google-chrome-stable',
   chromeFlags: ['--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--no-sandbox'],
   cacheOptions: { // see https://github.com/kwhitley/apicache
