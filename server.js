b3a9da9f2f02c8e7c985855a1b3f9103bdefe1bb
Thomas Burke <etburke4@gmail.com>
2018-01-10 15:16:35 -0800

    blockResources

--- server.js
+++ server.js
@@ -5,21 +5,22 @@ var prerender = require('./lib');
 var REDIS_URL = process.env.REDIS_URL;
 
 var server = prerender({
-  // logRequests: true,
-  // chromeLocation: '/usr/bin/google-chrome-stable',
-  // chromeFlags: ['--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--no-sandbox'],
+  logRequests: true,
+  chromeLocation: '/usr/bin/google-chrome-stable',
+  chromeFlags: ['--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--no-sandbox'],
   cacheOptions: { // see https://github.com/kwhitley/apicache
-    // debug: true,
     defaultDuration: '12 hours',
     redisClient: redis.createClient({
-      // host: REDIS_HOST,
-      // port: REDIS_PORT,
-      url: REDIS_URL || 'redis://localhost:6379'
+      url: REDIS_URL || 'redis://localhost:6379',
+      statusCodes: {
+        exclude: [500, 503, 504],
+      },
     }),
   },
 });
 
 server.use(prerender.sendPrerenderHeader());
+server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
