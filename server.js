3190fb902ee4213a4123a28fa91438f8d238263a
Thomas Burke <etburke4@gmail.com>
2018-01-10 14:25:19 -0800

    Added redis cache support

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 #!/usr/bin/env node
+var redis = require('redis');
 var prerender = require('./lib');
 
+var REDIS_URL = process.env.REDIS_URL;
+
 var server = prerender({
-  chromeLocation: '/usr/bin/google-chrome-stable',
-  chromeFlags: ['--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--no-sandbox']
+  // logRequests: true,
+  // chromeLocation: '/usr/bin/google-chrome-stable',
+  // chromeFlags: ['--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--no-sandbox'],
+  cacheOptions: { // see https://github.com/kwhitley/apicache
+    // debug: true,
+    defaultDuration: '12 hours',
+    redisClient: redis.createClient({
+      // host: REDIS_HOST,
+      // port: REDIS_PORT,
+      url: REDIS_URL || 'redis://localhost:6379'
+    }),
+  },
 });
 
 server.use(prerender.sendPrerenderHeader());
-// server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
